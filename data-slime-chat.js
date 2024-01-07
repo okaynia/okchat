c6917b24b4b995c884849fa449af9f8828ca9bc4
oknia <occasionallyoknia@gmail.com>
2024-01-06 16:24:26 -0800

    Added custom css

---
+++ data-slime-chat.js
@@ -0,0 +1,83 @@
+slime2.widget.setData({
+  "animations": {
+    "enter": "none",
+    "exit": "none"
+  },
+  "badges": {
+    "display": true,
+    "size": 24
+  },
+  "display": {
+    "direction": "vertical",
+    "maxWidth": 500,
+    "corner": "bottom-left",
+    "under": false,
+    "expiration": 30,
+    "max": 100,
+    "delay": 0,
+    "gap": 10
+  },
+  "emotes": {
+    "static": false,
+    "dynamic": true,
+    "medium": 56,
+    "large": 112
+  },
+  "filters": {
+    "botFilters": {
+      "names": [
+        "Sery_Bot",
+        "Nightbot",
+        "StreamElements",
+        "Streamlabs"
+      ],
+      "prefixes": [
+        "!"
+      ]
+    },
+    "userFilters": {
+      "types": [
+        "all",
+        "subs",
+        "mods",
+        "vips",
+        "followers",
+        "artists"
+      ],
+      "followHours": null,
+      "users": []
+    },
+    "messageFilters": {
+      "types": [],
+      "words": []
+    }
+  },
+  "pronouns": {
+    "display": "lowercase"
+  },
+  "sound": {
+    "audio": null,
+    "volume": 50
+  },
+  "backgroundStyles": {
+    "color": "transparent",
+    "padding": 0,
+    "fullWidth": false,
+    "borderColor": "transparent",
+    "borderWidth": 0,
+    "borderRadius": 0,
+    "borderStyle": "solid"
+  },
+  "textStyles": {
+    "font": "Open Sans",
+    "size": 20,
+    "lineHeight": 30,
+    "weight": 600,
+    "colon": true,
+    "usernameColorOption": "user-light",
+    "usernameColor": "white",
+    "textColor": "white",
+    "edge": "none",
+    "edgeColor": "black"
+  }
+})
